3e0c7466be66d2e1376fd147e39e36e7070dc850
Danny <drench+github@gmail.com>
2009-11-01 20:41:40 -0600

    initial, skeletal version

---
+++ lib/Library/Catalog/CCS.pm
@@ -0,0 +1,13 @@
+package Library::Catalog::CCS;
+
+our $VERSION = 0.01;
+
+use strict;
+use integer;
+use warnings;
+
+use Carp 'croak';
+use DateTime ();
+use WWW::Mechanize ();
+
+1;
---
+++ t/0.t
@@ -0,0 +1,5 @@
+# vim: se ft=perl :
+
+use Test::More tests => 1;
+
+use_ok('Library::Catalog::CCS');
