ac84f9c462794aa98fedc3a0df4a3bcb5ae5f3be
Danny <drench+github@gmail.com>
2009-11-02 20:19:02 -0600

    login() works

--- lib/Library/Catalog/CCS.pm
+++ lib/Library/Catalog/CCS.pm
@@ -10,4 +10,43 @@ use Carp 'croak';
 use DateTime ();
 use WWW::Mechanize ();
 
+our $BASEURL = 'http://64.107.155.140/cgi-bin/ibistro';
+
+sub new {
+    my $class = shift;
+    my $self = shift || {};
+    $self->{mech} = WWW::Mechanize->new();
+    return bless $self, $class;
+}
+
+sub login {
+    my $self = shift;
+
+    if (! defined $self->{card}) {
+        croak('No card number; cannot log in!');
+    }
+
+    $self->{card} =~ s/\D+//gs;
+    if (! length $self->{card}) {
+        croak('Invalid card number; cannot log in!');
+    }
+
+    $self->{password} = 'Patron' if ! defined $self->{password};
+
+    my $resp = $self->{mech}->get($BASEURL);
+    croak($resp->as_string) if $resp->is_error;
+
+    $resp = $self->{mech}->submit_form(
+        form_name => 'loginform',
+        fields    => {
+            user_id  => $self->{card},
+            password => $self->{password},
+        },
+        button    => 'loginbutton',
+    );
+    croak($resp->as_string) if $resp->is_error;
+
+    return $resp;
+}
+
 1;
--- t/0.t
+++ t/0.t
@@ -1,5 +1,19 @@
 # vim: se ft=perl :
 
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 use_ok('Library::Catalog::CCS');
+
+if ($ENV{CARD}) {
+    $ENV{PASS} = 'Patron' if ! defined $ENV{PASS};
+
+    my $ccs = Library::Catalog::CCS->new({
+        card => $ENV{CARD}, password => $ENV{PASS}
+    });
+
+    ok($ccs->login()->is_success);
+}
+else {
+    warn "Not testing login\n";
+    ok(1);
+}
