420b47bdf03cd7ff2b0c7db553699df73c305395
Danny <drench+github@gmail.com>
2009-11-02 20:46:11 -0600

    Start adding renewal code

--- lib/Library/Catalog/CCS.pm
+++ lib/Library/Catalog/CCS.pm
@@ -8,6 +8,7 @@ use warnings;
 
 use Carp 'croak';
 use DateTime ();
+use Library::Catalog::CCS::Item ();
 use WWW::Mechanize ();
 
 our $BASEURL = 'http://64.107.155.140/cgi-bin/ibistro';
@@ -47,6 +48,111 @@ sub login {
     croak($resp->as_string) if $resp->is_error;
 
     return $resp;
+}
+
+sub click_link {
+    my $self = shift;
+
+    my $link = $self->{mech}->find_link(@_);
+    my $resp = $self->{mech}->get($link->[0]);
+    croak($resp->as_string) if $resp->is_error;
+
+    return $resp;
+}
+
+sub get_renewable_items {
+    my $self = shift;
+
+    $self->login();
+    $self->click_link(text_regex => qr{\bMy\sAccount\b});
+    my $resp = $self->click_link(text_regex => qr{\bRenew\sMy\sMaterials\b});
+
+    return $self->parse_renewal_screen($resp);
+}
+
+sub parse_renewal_screen {
+    my $self = shift;
+    my $resp = shift;
+    my $html = $resp->content();
+
+    my $p = HTML::TokeParser->new(\$html);
+
+    my @items;
+
+    while (my $td = $p->get_token) {
+        next if $td->[0] ne 'S';
+        next if $td->[1] ne 'td';
+        next if $td->[2]->{class} !~ /^itemlisting/;
+
+        my $item = Library::Catalog::CCS::Item->new({parent => $self});
+   
+        my $input;
+        while ($input = $p->get_token) {
+            next if $input->[0] ne 'S';
+            next if $input->[1] ne 'input';
+            next if $input->[2]->{type} ne 'checkbox';
+            $item->{checkbox} = $input->[2]->{name};
+            last;
+        }
+   
+        while (my $label = $p->get_token) {
+            next if $label->[0] ne 'S';
+            next if $label->[1] ne 'label';
+            next if $label->[2]->{'for'} ne $input->[2]->{id};
+            last;
+        }
+   
+        my $lt = q{};
+
+        while (my $lf = $p->get_token) {
+            last if ($lf->[0] eq 'E') && ($lf->[1] eq 'label');
+
+            if ($lf->[0] eq 'T') {
+                $lt .= $lf->[1];
+            }
+        }
+        for ($lt) {
+            s/&nbsp;&nbsp;/: /gs;
+            s/\s+/ /gs;
+            s/^\s+//;
+            s/\s+$//;
+        }
+
+        $item->{label} = $lt;
+
+        while (my $dd = $p->get_token) {
+            next if $dd->[0] ne 'S';
+            next if $dd->[1] ne 'strong';
+            last;
+        }
+   
+        my $duedate = '';
+        while (my $dd = $p->get_token) {
+            last if ($dd->[0] eq 'E') && ($dd->[1] eq 'strong');
+            next if $dd->[0] ne 'T';
+            $duedate .= $dd->[1];
+        }
+   
+        if ($duedate =~ m{(\d{1,2})/(\d{1,2})/(\d{4}),(\d\d):(\d\d)}) {
+            $item->{duedate} = DateTime->new(
+                year      => $3,
+                month     => $1,
+                day       => $2,
+                hour      => $4,
+                minute    => $5,
+                second    => 0,
+                time_zone => 'America/Chicago',
+            );
+        }
+        else {
+            croak("Bad duedate '$duedate'");
+        }
+
+        push @items, $item;
+
+    }
+
+    return @items;
 }
 
 1;
---
+++ lib/Library/Catalog/CCS/Item.pm
@@ -0,0 +1,30 @@
+package Library::Catalog::CCS::Item;
+
+use strict;
+use integer;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $arg = shift || {};
+    return bless $arg, $class;
+}
+
+sub renew {
+    my $self = shift;
+
+    $self->{parent}->login();
+    $self->{parent}->click_link(text_regex => qr{\bMy\sAccount\b});
+    $self->{parent}->click_link(text_regex => qr{\bRenew\sMy\sMaterials\b});
+
+    my $resp = $self->{parent}{mech}->submit_form(
+        form_name => 'renewitems',
+        fields    => {
+            $self->{parent}{checkbox} => 'on',
+        },
+    );
+    croak($resp->as_string) if $resp->is_error;
+    return $resp;
+}
+
+1;
