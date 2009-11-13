309ae55574116803a15fa775eeaf4f8a690d9608
Danny <drench+github@gmail.com>
2009-11-13 15:01:45 -0600

    Extract some info from the renewal result screen

--- lib/Library/Catalog/CCS/Item.pm
+++ lib/Library/Catalog/CCS/Item.pm
@@ -4,10 +4,67 @@ use strict;
 use integer;
 use warnings;
 
+BEGIN { $::{parse_ccsdate} = $Library::Catalog::CCS::{parse_ccsdate} }
+
 sub new {
     my $class = shift;
     my $arg = shift || {};
     return bless $arg, $class;
+}
+
+sub parse_renewal_result {
+    my $self = shift;
+    my $resp = shift;
+    my $html = $resp->content();
+
+    my $p = HTML::TokeParser->new(\$html);
+
+    # find "Renewal Results: 1 item was renewed"
+    while (my $t = $p->get_token) {
+        next unless ($t->[0] eq 'S') && ($t->[1] eq 'strong');
+
+        my $txt = '';
+        while ($t = $p->get_token) {
+            last if ($t->[0] eq 'E') && ($t->[1] eq 'strong');
+            next unless $t->[0] eq 'T';
+            $txt .= $t->[1];
+        }
+
+        for ($txt) { s/[\r\n]+/ /gs; s/\s+/ /gs; s/^\s+//; s/\s+$//; }
+
+# FIX: make sure $txt really contains what we expected
+        carp($txt);
+        last;
+    }
+
+    # find new due date and number of times renewed:
+    while (my $t = $p->get_token) {
+        next unless ($t->[0] eq 'S') && ($t->[1] eq 'dd');
+
+        my %info;
+        my $k;
+
+        while ($t = $p->get_token) {
+            last if ($t->[1] eq 'E') && ($t->[1] eq 'dd');
+            next unless $t->[0] eq 'T';
+            next if $t->[1] !~ /\S/;
+
+            if ($t->[1] =~ /\b(.+):\s/) {
+                $k = $1;
+            }
+            elsif (defined $k) {
+                $info{$k} = parse_ccsdate($t->[1]) || $t->[1];
+                $k = undef;
+            }
+        }
+
+        # %info should contain:
+        #  "Date renewed"
+        #  "Times renewed"
+        #  "Due"
+
+        return \%info;
+    }
 }
 
 sub renew {
@@ -26,7 +83,7 @@ sub renew {
         },
     );
     croak($resp->as_string) if $resp->is_error;
-    return $resp;
+    return $self->parse_renewal_result($resp);
 }
 
 1;
